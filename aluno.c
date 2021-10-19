45ab9bcf91dd98b592a01ee874d06165f35274e9
Mikael Botassi <elkamilive@gmail.com>
2021-10-19 16:24:47 -0300

    Formatado

--- aluno.c
+++ aluno.c
@@ -3,9 +3,11 @@
 /*
 Função de menu, esta função chama as outras funções dependendo da resposta do usuário.
 */
-void menu(hashAberto *h){
-    int res=5;
-    while(res !=4){
+void menu(hashAberto *h)
+{
+    int res = 5;
+    while (res != 4)
+    {
         printf("\n 1 - EXIBIR ESTATISTICAS;");
         printf("\n 2 - EFETUAR BUSCA POR MATRICULA;");
         printf("\n 3 - INCLUIR ALUNO;");
@@ -13,30 +15,34 @@ void menu(hashAberto *h){
 
         printf("\nDigite o numero correspondente a funcao que deseja utilizar: ");
         scanf(" %d", &res);
-        
+
         /*if(res==1){
             exibeEstatisticas(h1, h2);
         }*/
-        if(res==2){
+        if (res == 2)
+        {
             alunoSearch(h);
         }
 
-        if(res==3){
+        if (res == 3)
+        {
             newAluno(h);
         }
     }
 }
 
-aluno * allocAluno(){
-    aluno * p=(aluno *)malloc(sizeof(aluno));
-    p->matricula=0;
-    p->nota=0;
+aluno *allocAluno()
+{
+    aluno *p = (aluno *)malloc(sizeof(aluno));
+    p->matricula = 0;
+    p->nota = 0;
     strcpy(p->nome, "\0");
     return p;
 }
 
-void newAluno(hashAberto * h1){//Caso o usuário queira inserir um aluno novo sem leitura de arquivos
-    aluno * p = allocAluno();
+void newAluno(hashAberto *h1)
+{ //Caso o usuário queira inserir um aluno novo sem leitura de arquivos
+    aluno *p = allocAluno();
     printf("\nDigite a matricula do aluno: ");
     scanf("%d", &p->matricula);
     printf("Digite o nome do aluno: ");
@@ -46,88 +52,110 @@ void newAluno(hashAberto * h1){//Caso o usuário queira inserir um aluno novo se
     insertAluno(h1, p);
 }
 
-hashAberto* insertAluno(hashAberto *h, aluno * a){
+hashAberto *insertAluno(hashAberto *h, aluno *a)
+{
     return inserirNaHashAberta('A', h, a, returnChave);
 }
 
-int descobreTipo(char id){
-/*
+int descobreTipo(char id)
+{
+    /*
 Esta função pega o tipo de dado passado pelo variavel, neste caso
 como quero trabalhar apenas com aluno, recdbo apenas o tipo 'A' ou 'a'
 */
-    if(id =='A' || id =='a'){
+    if (id == 'A' || id == 'a')
+    {
         return 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
 
-void alunoSearch(hashAberto *h){//Procura o aluno pela matricula
+void alunoSearch(hashAberto *h)
+{ //Procura o aluno pela matricula
     int chave;
     printf("\nQual a matricula da pessoa que voce quer procurar? ");
     scanf(" %d", &chave);
     elemento *e = pesquisaNaHash(h, chave, matriculaCompare);
-    if(e!=NULL){
+    if (e != NULL)
+    {
         printAluno(e->tipo, e->valor);
     }
-    else{
+    else
+    {
         printf("\nALUNO NAO EXISTE!\n");
     }
 }
 
-int matriculaCompare(int matricula, elemento * elem){//função de comparar pra ver se as matriculas são iguais
-    if(descobreTipo(elem->tipo)){
-        aluno *p=elem->valor;
-        if(p->matricula==matricula){
+int matriculaCompare(int matricula, elemento *elem)
+{ //função de comparar pra ver se as matriculas são iguais
+    if (descobreTipo(elem->tipo))
+    {
+        aluno *p = elem->valor;
+        if (p->matricula == matricula)
+        {
             return 1;
         }
-        return 0;    
+        return 0;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
 
-void printAluno(char id, void *elem){
-    if(descobreTipo(id)){
+void printAluno(char id, void *elem)
+{
+    if (descobreTipo(id))
+    {
         aluno *p = elem;
         printf("\nMatricula: %d", p->matricula);
         printf("\nNome: %s", p->nome);
         printf("\nNota: %d", p->nota);
         printf("\n");
-
     }
-    else{//Caso a variavel não seja do tipo aluno
+    else
+    { //Caso a variavel não seja do tipo aluno
         printf("\nNao e possivel printar! ");
     }
 }
 
-void printIndice(elemento *elem, int indice){
-    if(descobreTipo(elem->tipo)){
+void printIndice(elemento *elem, int indice)
+{
+    if (descobreTipo(elem->tipo))
+    {
         aluno *p = elem->valor;
         printf("\n Indice = %d - Matricula = %d - Nome = %s\n", indice, p->matricula, p->nome);
     }
-    else{//Caso a variavel não seja do tipo aluno
+    else
+    { //Caso a variavel não seja do tipo aluno
         printf("\nDigite mais sentencas! ");
     }
 }
 
-void printAllHash(hashAberto *h){
-    if(h->tamanho==0){
+void printAllHash(hashAberto *h)
+{
+    if (h->tamanho == 0)
+    {
         printf("Hash vazia!\n");
     }
-    else{
+    else
+    {
         imprimeHash(h, printIndice);
     }
 }
 
-int returnChave(void * elem, char id){
-    if(descobreTipo(id)){// É do tipo aluno?
-        aluno * a = elem;
-        return a->matricula;//Se sim retorna a matricula do aluno, a "chave" para a inserção
+int returnChave(void *elem, char id)
+{
+    if (descobreTipo(id))
+    { // É do tipo aluno?
+        aluno *a = elem;
+        return a->matricula; //Se sim retorna a matricula do aluno, a "chave" para a inserção
     }
-    else{
+    else
+    {
         printf("\nDigite mais setenças de tipos de chaves! \n");
         return 0;
     }
--- controleArquivo.c
+++ controleArquivo.c
@@ -14,38 +14,41 @@
 
 // =-=-=-=-= METODOS PRIVADOS | DECLARAÇÃO =-=-=-=-=
 
-aluno * readNextAluno(FILE *fp);
+aluno *readNextAluno(FILE *fp);
 
 void writeAlunosOnFile(FILE *fp, hashAberto *hash);
 
 // =-=-=-=-= METODOS PRIVADOS | IMPLEMENTAÇÃO =-=-=-=-=
 
-aluno * readNextAluno(FILE *fp) {
-    aluno * aluno = allocAluno();
-    char *line = (char *) malloc((LINE_MAX_LENGTH + 1) * sizeof(char));
+aluno *readNextAluno(FILE *fp)
+{
+    aluno *aluno = allocAluno();
+    char *line = (char *)malloc((LINE_MAX_LENGTH + 1) * sizeof(char));
     char *ptr;
 
-    
     fscanf(fp, " %[^\n]%*c", line);
     ptr = strtok(line, DELIMITER);
-    aluno->matricula = (int) atof(ptr);
+    aluno->matricula = (int)atof(ptr);
 
     ptr = strtok(NULL, DELIMITER);
     strcpy(aluno->nome, ptr);
 
     ptr = strtok(NULL, DELIMITER);
-    aluno->nota = (int) atof(ptr);
+    aluno->nota = (int)atof(ptr);
 
     free(line);
 
     return aluno;
 }
 
-void writeAlunosOnFile(FILE *fp, hashAberto *hash) {
-    aluno * a;
+void writeAlunosOnFile(FILE *fp, hashAberto *hash)
+{
+    aluno *a;
     elemento *e;
-    for (int index = 0; index < hash->tamanho; ++index) {
-        if (hash->tabela[index].situacao==1){
+    for (int index = 0; index < hash->tamanho; ++index)
+    {
+        if (hash->tabela[index].situacao == 1)
+        {
             e = &(hash->tabela[index]);
             a = e->valor;
             fprintf(fp, "%d;%d;%s;%d\n", index, a->matricula, a->nome, a->nota);
@@ -55,35 +58,40 @@ void writeAlunosOnFile(FILE *fp, hashAberto *hash) {
 
 // =-=-=-=-= METODOS PUBLICOS =-=-=-=-=
 
-hashAberto* readHashAlunoFromFile(hashAberto *hash1) {
+hashAberto *readHashAlunoFromFile(hashAberto *hash1)
+{
     FILE *fp = fopen(DIRETORIO_ARQUIVO_ENTRADA, "r");
     int contador = 0;
     int registros;
 
-    if (!fp) {
+    if (!fp)
+    {
         printf(ERRO_ABRIR_ARQUIVO);
         return NULL;
     }
 
     printf(MENSSAGE_LENDO_ARQUIVO);
     fscanf(fp, "%d", &registros);
-    while (!feof(fp) && contador < registros) {
-        aluno * a = readNextAluno(fp);
-        hash1= insertAluno(hash1, a);
+    while (!feof(fp) && contador < registros)
+    {
+        aluno *a = readNextAluno(fp);
+        hash1 = insertAluno(hash1, a);
         contador++;
     }
     fclose(fp);
     return hash1;
 }
 
-void writeHashOnFile(hashAberto *hash) {
+void writeHashOnFile(hashAberto *hash)
+{
     int nomeArquivoSaidaLength = strlen(DIRETORIO_ARQUIVO_SAIDA) + FILE_NAME_MAX_LENGTH + 1;
-    char *nomeArquivoSaida = (char *) malloc(nomeArquivoSaidaLength * sizeof(char));
+    char *nomeArquivoSaida = (char *)malloc(nomeArquivoSaidaLength * sizeof(char));
     FILE *fp;
 
     sprintf(nomeArquivoSaida, "%ssaida-%d.txt", DIRETORIO_ARQUIVO_SAIDA, hash->tamanho);
     fp = fopen(nomeArquivoSaida, "w");
-    if (!fp) {
+    if (!fp)
+    {
         printf(ERRO_ABRIR_ARQUIVO);
         return;
     }
